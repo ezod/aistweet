56d8b7a0c22df127feea57365f2e34b2b3511de9
Aaron Mavrinac <mavrinac@gmail.com>
2021-01-22 21:03:04 +0000

    Import units module.

---
+++ aistweet/units.py
@@ -0,0 +1,11 @@
+import math
+
+
+def kn_to_m_s(knots):
+    """Convert knots to meters per second."""
+    return 0.514444444 * knots
+
+
+def m_to_lon(meters, latitude):
+    """Convert meters to degrees longitude (approximate)."""
+    return meters / (math.cos(math.radians(latitude)) * 111111.0)
