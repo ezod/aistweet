e94defda47896e53bfbb9613bf0331e64658a573
Aaron Mavrinac <mavrinac@gmail.com>
2021-01-28 18:40:15 +0000

    Add crossing check and scheduler to Tweeter.

--- aistweet/tweeter.py
+++ aistweet/tweeter.py
@@ -1,7 +1,9 @@
 import os
+import threading
 import time
 
 import tweepy
+from event_scheduler import EventScheduler
 from picamera import PiCamera
 
 
@@ -9,9 +11,22 @@ class Tweeter(object):
     CAMERA_WARMUP = 1.0
 
     def __init__(
-        self, tracker, consumer_key, consumer_secret, access_token, access_token_secret
+        self,
+        tracker,
+        direction,
+        consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret,
     ):
         self.tracker = tracker
+
+        self.direction = direction
+
+        self.schedule = {}
+        self.scheduler = EventScheduler("tweeter")
+
+        self.lock = threading.RLock()
 
         # set up camera
         self.camera = PiCamera()
@@ -21,25 +36,50 @@ class Tweeter(object):
         auth.set_access_token(access_token, access_token_secret)
         self.twitter = tweepy.API(auth)
 
+        self.scheduler.start()
+
+        # register callback
+        self.tracker.message_callbacks.append(self.check)
+
+    def check(self, mmsi, t):
+        crossing = self.tracker.crossing_time(mmsi, self.direction)
+        if crossing is None:
+            return
+        delta = crossing - time.time()
+        if not mmsi in self.schedule and delta < self.CAMERA_WARMUP + 0.5:
+            snap_and_tweet(self, mmsi)
+        if self.CAMERA_WARMUP + 0.5 < delta < 10.0:
+            existing_event = self.schedule.pop(mmsi)
+            if existing_event:
+                self.scheduler.cancel(existing_event)
+            self.schedule[mmsi] = self.scheduler.enterabs(
+                crossing - self.CAMERA_WARMUP - time.time() + time.monotonic(),
+                1,
+                self.snap_and_tweet,
+                argument=(mmsi,),
+            )
+
     def snap_and_tweet(self, mmsi):
-        # grab the image
-        image_path = os.path.join("/tmp", "{}.jpg".format(mmsi))
-        self.snap(image_path)
+        with self.lock:
+            # grab the image
+            image_path = os.path.join("/tmp", "{}.jpg".format(mmsi))
+            self.snap(image_path)
 
-        # tweet the image with info
-        lat, lon = self.tracker.center_coords()
-        self.twitter.update_with_media(
-            image_path, self.generate_text(mmsi), lat=lat, long=lon
-        )
+            # tweet the image with info
+            lat, lon = self.tracker.center_coords()
+            self.twitter.update_with_media(
+                image_path, self.generate_text(mmsi), lat=lat, long=lon
+            )
 
-        # clean up the image
-        os.remove(image_path)
+            # clean up the image
+            os.remove(image_path)
 
     def snap(self, path):
-        # TODO: get sunrise/sunset (astral?) and adjust exposure
-        self.camera.start_preview()
-        time.sleep(CAMERA_WARMUP)
-        self.camera.capture(path)
+        with self.lock:
+            # TODO: get sunrise/sunset (astral?) and adjust exposure
+            self.camera.start_preview()
+            time.sleep(self.CAMERA_WARMUP)
+            self.camera.capture(path)
 
     def generate_text(self, mmsi):
         text = u"{} ".format(self.tracker.flag(mmsi))
