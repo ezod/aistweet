2147db1ac5af64d2df0dcd0b8718e72700a28ed6
Aaron Mavrinac <mavrinac@gmail.com>
2021-07-20 11:46:24 -0400

    Remove instant snap and tweet for small delta (always use scheduler), to avoid multiple tweet bug. Closes #6.

--- aistweet/tweeter.py
+++ aistweet/tweeter.py
@@ -78,9 +78,7 @@ class Tweeter(object):
         if crossing is None:
             return
         delta = crossing - time.time() - self.CAMERA_WARMUP - self.CAMERA_DELAY
-        if not mmsi in self.schedule and delta < 0.5:
-            self.snap_and_tweet(mmsi)
-        elif 0.5 < delta < 60.0:
+        if 0.0 < delta < 60.0:
             try:
                 existing_event = self.schedule.pop(mmsi)
                 self.scheduler.cancel(existing_event)
